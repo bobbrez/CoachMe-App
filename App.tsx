94d1c8b012a6a984002318270622e4ba94bd9309
Bob Breznak <bob.breznak@gmail.com>
2020-07-08 10:56:53 -0400

    Refactor HomeScreen component (and sub-components) into separate definition

--- App.tsx
+++ App.tsx
@@ -1,27 +1,9 @@
-
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Button, IconRegistry, Icon, IconProps } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from './theme.json';
-
-const LoginIcon = (props : IconProps) => (
-  <Icon name='log-in-outline' {...props} />
-);
-
-const LoginButton = () => (
-  <Button style={{ marginTop: 10 }} accessoryLeft={LoginIcon}>Log In</Button>
-);
-
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h3'>Welcome to CoachMe</Text>
-    <Text>Log in to get started</Text>
-    <LoginButton />
-    <StatusBar style="auto" />
-  </Layout>
-)
+import HomeScreen from './components/HomeScreen';
 
 export default () => (
   <>
