9e10a7e02b4878268d006c8105105d973fca482b
Bob Breznak <bob.breznak@gmail.com>
2020-07-08 10:45:34 -0400

    Add basic LoginButton

--- App.tsx
+++ App.tsx
@@ -2,7 +2,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
+
+const LoginButton = () => (
+  <Button>Log In</Button>
+);
 
 export default function App() {
   return (
@@ -10,6 +14,7 @@ export default function App() {
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text category='h3'>Welcome to CoachMe</Text>
         <Text>Log in to get started</Text>
+        <LoginButton />
         <StatusBar style="auto" />
       </Layout>
     </ApplicationProvider>
