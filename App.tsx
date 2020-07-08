265e863f45ca0b84cf21418783c19b870f0c5daf
Bob Breznak <bob.breznak@gmail.com>
2020-07-08 10:48:01 -0400

    Add log-in icon to Login button

--- App.tsx
+++ App.tsx
@@ -2,21 +2,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text, Button } from '@ui-kitten/components';
+import { ApplicationProvider, Layout, Text, Button, IconRegistry, Icon } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+const LoginIcon = (props) => (
+  <Icon name='log-in-outline' {...props} />
+);
 
 const LoginButton = () => (
-  <Button>Log In</Button>
+  <Button accessoryLeft={LoginIcon}>Log In</Button>
 );
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text category='h3'>Welcome to CoachMe</Text>
-        <Text>Log in to get started</Text>
-        <LoginButton />
-        <StatusBar style="auto" />
-      </Layout>
-    </ApplicationProvider>
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={{ ...eva.dark }}>
+        <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text category='h3'>Welcome to CoachMe</Text>
+          <Text>Log in to get started</Text>
+          <LoginButton />
+          <StatusBar style="auto" />
+        </Layout>
+      </ApplicationProvider>
+    </>
   );
 }
